c1280856e0445c76bda0967364e9020464869d89
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2017-06-13 10:59:08 +0300

    fix implementation

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
-import kotlinx.coroutines.experimental.*
-import kotlinx.coroutines.experimental.channels.*
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.channels.ActorJob
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.SendChannel
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.runBlocking
 import kotlin.system.measureTimeMillis
 
 sealed class Msg
@@ -20,32 +25,31 @@ fun create() = actor<Msg>(CommonPool) {
     }
 }
 
-suspend fun add(actor: ActorJob<Msg>, n: Long) = actor.send(Add(n))
-
 suspend fun getAndReset(actor: ActorJob<Msg>): Long {
     val reply = Channel<Long>()
     actor.send(GetAndReset(reply))
     return reply.receive()
 }
 
-val processorCount = 8
-
 suspend fun run(numPerThread: Int) {
-    print("ChMsg: ")
+    val processorCount = Runtime.getRuntime().availableProcessors()
+
     val elapsed = measureTimeMillis {
         val actor = create()
-        for (i in 1..processorCount) {
-            for (n in 1..numPerThread) {
-                add(actor, 100L)
+        (1..processorCount).map {
+            async(CommonPool) {
+                for (n in 1..numPerThread) {
+                    actor.send(Add(100L))
+                }
                 getAndReset(actor)
             }
-        }
+        }.forEach { it.await() }
     }
     print("$processorCount * $numPerThread msgs => ${processorCount.toDouble() * numPerThread / (elapsed / 1000.0)} msgs/s\n")
 }
 
 fun main(args: Array<String>) = runBlocking {
-    for (n in listOf(300, 3000, 30000, 300000, 3000000)) {
+    for (n in listOf(300, 3_000, 30_000, 300_000, 3_000_000)) {
         run(n)
     }
 }
